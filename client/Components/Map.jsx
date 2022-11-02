411830fab3453ffc4bcf37722bab39f07a639a08
Melanie Forbes <mforbes12@gmail.com>
2022-11-01 17:41:18 -0700

    add location button

--- client/Components/Map.jsx
+++ client/Components/Map.jsx
@@ -2,12 +2,23 @@ import React, { Component } from 'react';
 import { render } from 'react-dom';
 
 
+
 class Map extends Component {
+  constructor(props) {
+    super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  };
+
+  handleSubmit() {
+    console.log("Hey!")
+  };
+
   render() {
     let imgSrc = 'https://www.freeworldmaps.net/printable/worldmap-countries-hd.jpg';
     return (
       <div className ="map">
         <h1 className ="title">Welcome to your Next Adventure</h1>
+        <button className="add" onClick={this.handleSubmit}>Add New Adventure</button>
         <img className ="staticMap" src={imgSrc} alt='world map' />
       </div>
     )
--- server/locationModels.js
+++ server/locationModels.js
@@ -1,9 +1,12 @@
+cpmst mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
+const pinSchema = new Schema({
+  city: {type: String, required:true},
+  country: {type: String, required: true},
+  type: {type: String},
+  recommendation: {type: String, required: true},
+  recommended_by: {type: String}
+});
 
-//schema collections: location
-//{lat: ,
-//lon: ,
-//type: ,
-//message: ,
-//recommended_by: ,
-//}
+module.exports = mongoose.model('pin', pinSchema);
